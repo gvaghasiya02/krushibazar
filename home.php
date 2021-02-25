bb61f751e39a75913d2762740ebac2aebe2ad551
gv <krushibazarggh@gmail.com>
2021-02-25 11:03:02 +0530

    gv

--- home.php
+++ home.php
@@ -19,12 +19,15 @@ session_start();?>
 </div>
 <nav class="navbar navbar-default">
   <div class="container-fluid">
-    <ul class="nav navbar-nav">
+    <ul class="nav navbar-nav mr-auto">
       <li class="active"><a href="home.php">Home</a></li>
       <li><a href="sale.php">Selling Crops</a></li>
       <li><a href="buying.php">Buying Crops</a></li>
       <li><a href="pesticides.php">Buying Pesticides</a></li>
       <li><a href="profile.php">logged in as:<?php echo $_SESSION['email'];?></a></li>
+      </ul>
+      <ul class="nav navbar-nav">
+      <li><a href="logout.php">Logout</a></li>
     </ul>
   </div>
 </nav>
@@ -32,6 +35,6 @@ session_start();?>
 This section provides the information on agriculture produces; machineries, research etc. Detailed information on the government policies, schemes, agriculture loans, market prices, animal husbandry, fisheries, horticulture, loans & credit, sericulture etc. is also available.
 </p>
 <h4>Important links</h4> 	  <ul> 	    <li><a href="/website-ministry-agriculture-farmers-welfare">Website of Ministry of Agriculture &amp; Farmers Welfare</a></li><li><a href="/departments-agriculture-states-and-union-territories">Departments of Agriculture of states and Union Territories</a></li><li><a href="/website-department-animal-husbandry-dairying-and-fisheries-0">Website of Department of Animal Husbandry Dairying and Fisheries</a></li><li><a href="/website-directorate-cashewnut-cocoa-development">Website of Directorate of Cashewnut &amp; Cocoa Development</a></li><li><a href="/farmers-portal-india-department-agriculture-and-cooperation">Farmers&#039; Portal of India by Department of Agriculture and Cooperation</a></li>	  </ul> 
-<a href="logout.php">Logout</a>
+
 </body>
 </html>
