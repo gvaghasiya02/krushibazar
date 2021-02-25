a79e8c74d712f698ef74db4610c851b8c5691fdf
gv <krushibazarggh@gmail.com>
2021-02-25 09:29:48 +0530

    Gv changed

--- home.php
+++ home.php
@@ -4,10 +4,31 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <title>Home</title>
 </head>
 <body>
-    <h1>Welcome</h1>
-    <a href="logout.php">Logout</a>
+<h1 align="center"><img src="https://i.pinimg.com/originals/ef/9c/42/ef9c4284f56e717c8dd191844af07908.png" height=250 width=auto></h1>
+<div class="container">
+
+  <h1 align="center">Welcome to Krushibazar</h1>
+</div>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="home.php">Krushibazar</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="home.php">Home</a></li>
+      <li><a href="sale.php">Selling Crops</a></li>
+      <li><a href="buying.php">Buying Crops</a></li>
+      <li><a href="pesticides.php">Buying Pesticides</a></li>
+    </ul>
+  </div>
+</nav>
+<p>
+<a href="logout.php">Logout</a>
 </body>
 </html>
