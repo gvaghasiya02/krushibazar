524252d722fe42841d7541e90233e4ac7ce804b8
gv <krushibazarggh@gmail.com>
2021-02-26 09:18:37 +0530

    gv

--- sale.php
+++ sale.php
@@ -9,6 +9,7 @@ session_start();?>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <script src="https://cdn.ckeditor.com/4.8.0/full/ckeditor.js"></script>
     <title>Selling</title>
 </head>
 <body>
@@ -33,14 +34,44 @@ session_start();?>
 <h1 class="text-primary">Upload crop</h1>
     <form action="" method="post">
         <div class="form-row">
-            <div class="form-group col-md-4">
+            <div class="form-group col-md-6">
                 <label for="cname">Crop name</label>
                 <input class="form-control" type="text" name="cname" id="cname" placeholder="Enter cname">
             </div>
-            <div class="form-group col-md-4">
-                <label for="price">Price</label>
-                <input class="form-control" type="text" name="price" id="price" placeholder="Enter Price">
+            <div class="form-group col-md-6">								  
+                  <div class="form-group">
+                  <label for="sel1">Select list (select one):</label>
+                  <select class="form-control" id="sel1">
+                  <option value="Fruit" style="color: black;">Fruit</option>
+								  <option value="Vegetable" style="color: black;">Vegetable</option>
+								  <option value="Grains" style="color: black;">Grains</option>
+                  </select>
+                  </div>
             </div>
         </div>
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <label for="price">Price</label>
+                <input class="form-control" type="number" name="price" id="price" placeholder="Enter Price">
+            </div>
+            <div class="form-group col-md-6">
+                <label for="croppic">Upload an Image</label>
+                <input class="form-control" type="file" name="croppic" id="croppic"></input>
+            </div>
+        </div>
+        <div class="form-row"> 
+        <div class="form-group col-md-10"> 
+					<textarea  name="cinfo" id="cinfo" rows="12"></textarea>
+        </div>
+				</div>
+        <div class="form-row"> 
+        <div class="form-group col-md-3"> 
+        <button type="button" class="btn btn-default">Upload</button>
+        </div>
+				</div>
+      </from>
+      <script>
+			 CKEDITOR.replace( 'cinfo' );
+		</script>
 </body>
 </html>
