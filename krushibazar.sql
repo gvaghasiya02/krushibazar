0529e50e08b4b8cacbab73134607c3f370c1ecbf
GV <gvaghasiya02@gmail.com>
2021-03-04 16:10:32 +0530

    gv

--- krushibazar.sql
+++ krushibazar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 04, 2021 at 10:50 AM
+-- Generation Time: Mar 04, 2021 at 11:40 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.0
 
