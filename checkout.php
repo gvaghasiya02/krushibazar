92be5f17ef958b281abb2605308f9df6a0668c49
GV <gvaghasiya02@gmail.com>
2021-03-07 12:38:55 +0530

    gv

--- checkout.php
+++ checkout.php
@@ -23,23 +23,15 @@ if($result->num_rows==1)
     $dob=$row['dob'];
 }
 $conn->close();
-if(isset($_POST["pay"]))
+
+if(isset($_POST["submit"]))
 {
   if(empty(trim($_POST['address']))|| empty(trim($_POST['city'])) || empty(trim($_POST['state'])))
   {
     $err.="Enter all detailsvsdv<br>";
-            $success1=false;
+            $success=false;
   }
-  else
-  {
-
-  }
-  echo $err1;
-}
-#echo date("F Y");
-if(isset($_POST["submit"]))
-{
-  if(empty(trim($_POST['cardname']))|| empty(trim($_POST['cardno'])) || empty(trim($_POST['date'])) || empty(trim($_POST['cvv'])) )
+  elseif(empty(trim($_POST['cardname']))|| empty(trim($_POST['cardno'])) || empty(trim($_POST['date'])) || empty(trim($_POST['cvv'])) )
   {
     $err.="Enter all details<br>";
             $success=false;
@@ -64,7 +56,7 @@ if(isset($_POST["submit"]))
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
+  <title>Payment</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -75,7 +67,7 @@ if(isset($_POST["submit"]))
 <body>
 <div class="container">
         <h1 class="text-center">Welcome to Krushibazar</h1>
-    </div>
+</div>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
             <ul class="navbar-nav mr-auto">
@@ -102,78 +94,71 @@ if(isset($_POST["submit"]))
             </div>";
         }
     ?>
+<form action="" method="post">
 <div class="container col-md-5 mt-10">
-<form action="" method="post" target="#payment">
-<div class="form-row">
-            <div class="form-group col-md-12">
-                <label for="address">Address</label>
-                <input class="form-control py-5" type="textarea" name="address" id="address" value=<?php echo $address;?> placeholder="Enter your Address" required>
+          <div class="form-row">
+              <div class="form-group col-md-12">
+                  <label for="address">Address</label>
+                  <input class="form-control py-5" type="textarea" name="address" id="address" value=<?php echo $address;?> placeholder="Enter your Address">
+              </div>
+          </div >
+          <div class="form-row">
+                <div class="form-group col-md-6">
+                        <label for="city">City</label>
+                        <input class="form-control" type="text" name="city" id="city" value=<?php echo $city;?> placeholder="Enter City">
+                </div>
+                <div class="form-group col-md-6">
+                        <label for="state">State</label>
+                        <input class="form-control" type="text" name="state" id="state" value=<?php echo $state;?> placeholder="Enter State">
+                </div>            
+          </div>
+          <!-- Button trigger modal -->
+          <button type="submit" class="btn btn-primary" data-toggle="modal" data-target="#payment">
+            Pay Through Card
+          </button>
+
+        <!-- Modal -->
+          <div class="modal fade" id="payment" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Enter card Details</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                        <div class="form-row">
+                        <div class="form-group col-md-12">
+                        <label for="cardname">Name on Card</label>
+                                  <input class="form-control" type="text" name="cardname" id="cardname" placeholder="Enter Name on card">
+                              </div>
+                              </div>
+                              <div class="form-row">
+                        <div class="form-group col-md-12">
+                        <label for="cardno">Card Number</label>
+                                  <input class="form-control" type="number" name="cardno" id="cardno" placeholder="Enter Card Number">
+                              </div>
+                              </div>
+                              <div class="form-row">
+                              <div class="form-group col-md-6">
+                              <label for="date">Expiration</label>
+                                  <input class="form-control" type="month" name="date" id="date" placeholder="Valid Date">
+                              </div>
+                              <div class="form-group col-md-6">
+                              <label for="cvv">Security Code</label>
+                                  <input class="form-control" type="password" name="cvv" id="cvv" placeholder="CVV">
+                              </div>                    
+                              </div>
+                        </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                  <button type="submit" name="submit" class="btn btn-primary">Proceed</button>
+                </div>
+              </div>
             </div>
-        </div >
-        <div class="form-row">
-        <div class="form-group col-md-6">
-                <label for="city">City</label>
-                <input class="form-control" type="text" name="city" id="city" value=<?php echo $city;?> placeholder="Enter City" required>
-            </div>
-            <div class="form-group col-md-6">
-                <label for="state">State</label>
-                <input class="form-control" type="text" name="state" id="state" value=<?php echo $state;?> placeholder="Enter State" required>
-            </div>
-           
-        </div>
-  <!-- Button trigger modal -->
-<button type="submit" class="btn btn-primary" name="pay" data-toggle="modal" data-target="#payment">
-  Pay Through Card
-</button>
+          </div> 
+</div>
 </form>
-<!-- Modal -->
-<div class="modal fade" id="payment" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Enter card Details</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-      <form method="post" action="">
-      <div class="form-row">
-      <div class="form-group col-md-12">
-      <label for="cardname">Name on Card</label>
-                <input class="form-control" type="text" name="cardname" id="cardname" placeholder="Enter Name on card">
-            </div>
-            </div>
-            <div class="form-row">
-      <div class="form-group col-md-12">
-      <label for="cardno">Card Number</label>
-                <input class="form-control" type="number" name="cardno" id="cardno" placeholder="Enter Card Number">
-            </div>
-            </div>
-            <div class="form-row">
-            <div class="form-group col-md-6">
-            <label for="date">Expiration</label>
-                <input class="form-control" type="month" name="date" id="date" placeholder="Valid Date">
-            </div>
-            <div class="form-group col-md-6">
-            <label for="cvv">Security Code</label>
-                <input class="form-control" type="password" name="cvv" id="cvv" placeholder="CVV">
-            </div>
-            
-            </div>
-      </div>
-      <div class="modal-footer">
-
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="submit" name="submit" class="btn btn-primary">Proceed</button>
-
-        </form>
-      </div>
-    </div>
-  </div>
-</div>
-  
-</div>
-
 </body>
 </html>
