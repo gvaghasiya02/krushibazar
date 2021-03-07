3d03a570509c0aa68dd23181de8d549d646d27b9
GV <gvaghasiya02@gmail.com>
2021-03-08 00:36:28 +0530

    gv pdf done

--- bill.php
+++ bill.php
@@ -19,14 +19,24 @@ $sql="SELECT `pid`,`qty` FROM `orderdetail` WHERE `orderid`='$oid'";
             array_push($cart,$row);
         }
     }
-    
+    $ss="<head>
+    <meta charset='UTF-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Cart</title>
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>
+    <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>
+    <script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossorigin='anonymous'></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js' integrity='sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1' crossorigin='anonymous'></script>
+    <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js' integrity='sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM' crossorigin='anonymous'></script>
+</head>";
             $data="
             <body>
             <div class='container mt-4 col-md-6'>
     <table class='table' align=center>
     <tr><th colspan=6 class='text-center'><h1>INVOICE</h1></th></tr>
         <tr>
-        <th colspan=3 class='text-left'>Order ID:".$oid."<br>TO:".$_SESSION['email']."<br>Delivery Address:<br> ".$delivery['daddress']."<br>". $delivery['dcity'].",".$delivery['dstate']."</th>
+        <th colspan=3 class='text-left'>Order ID: ".$oid."<br>TO: ".$_SESSION['email']."<br>Delivery Address:<br> ".$delivery['daddress']."<br>". $delivery['dcity'].",".$delivery['dstate']."</th>
         <th colspan=3 rowspan=3 class='text-right' scope='col'>". date('l jS \of F Y h:i:s A')."</th>
         </tr>
         </table>
@@ -145,6 +155,7 @@ $sql="SELECT `pid`,`qty` FROM `orderdetail` WHERE `orderid`='$oid'";
                         </tbody>
                 </table>";
                 $_SESSION['data']=$data;
+                $_SESSION['ss']=$ss;
                 ?>
                 <div class="d-flex flex-row-reverse bd-highlight my-4">
                     <!-- Button trigger modal -->
