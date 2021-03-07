85b81b25f91a14f85dd2d95f0fd8c4d8ba15c9d9
GV <gvaghasiya02@gmail.com>
2021-03-08 00:10:07 +0530

    gv

--- bill.php
+++ bill.php
@@ -19,36 +19,30 @@ $sql="SELECT `pid`,`qty` FROM `orderdetail` WHERE `orderid`='$oid'";
             array_push($cart,$row);
         }
     }
-    if(isset($_POST["savepdf"]))
-    {
-            require_once '../vendor/autoload.php';
-            $mpdf=new \Mpdf\Mpdf();
-            $data="<!DOCTYPE html>
-            <html lang='en'>
-            <head>
-                <meta charset='UTF-8'>
-                <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-                <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-                <title>Cart</title>
-                <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>
-                <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>
-                <script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossorigin='anonymous'></script>
-                <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js' integrity='sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1' crossorigin='anonymous'></script>
-                <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js' integrity='sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM' crossorigin='anonymous'></script>
-            </head>
+    
+            $data="
             <body>
             <div class='container mt-4 col-md-6'>
     <table class='table' align=center>
     <tr><th colspan=6 class='text-center'><h1>INVOICE</h1></th></tr>
         <tr>
         <th colspan=3 class='text-left'>Order ID:".$oid."<br>TO:".$_SESSION['email']."<br>Delivery Address:<br> ".$delivery['daddress']."<br>". $delivery['dcity'].",".$delivery['dstate']."</th>
-        <th colspan=3 rowspan=3 class='text-right' scope='col'><?php echo date('l jS \of F Y h:i:s A');?></th>
+        <th colspan=3 rowspan=3 class='text-right' scope='col'>". date('l jS \of F Y h:i:s A')."</th>
         </tr>
         </table>
+        <table class='table  table-striped' cellpadding=5px align=center>
+        <thead class='thead-dark'>
+            <tr>
+                <th class='text-center' scope='col'>Sr. No.</th>
+                <th class='text-center' scope='col'>Name</th>
+                <th class='text-center' scope='col'>Category</th>
+                <th class='text-center' scope='col'>Price</th>
+                <th class='text-center' scope='col'>Quantity</th>
+                <th class='text-center' scope='col'>Total</th>
+            </tr>
+        </thead>
+        <tbody>
         ";
-            $mpdf->WriteHTML($data);
-            $mpdf->Output('Invoice.pdf','D');
-    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -126,7 +120,16 @@ $sql="SELECT `pid`,`qty` FROM `orderdetail` WHERE `orderid`='$oid'";
                                 <th class="text-center"><?php echo $value['qty']?></th>                        
                                 <th class="text-center"><?php echo $item['price']*$value['qty']; ?></th>
                         </tr>
-                        <?php } ?>
+                        <?php 
+                            $data.="<tr>
+                            <th class='text-center'>". $srno ."</th>
+                                <th class='text-center'>". $item['pname']."</th>
+                                <th class='text-center'>". $item['category']."</th>
+                                <th class='text-center'>". $item['price']."</th>
+                                <th class='text-center'>". $value['qty']."</th>                        
+                                <th class='text-center'>". $item['price']*$value['qty']."</th>
+                        </tr>";
+                        } ?>
                         <tr>
                             <th colspan=4></th>
                             <th class="text-center">Total<br>Quantity: <?php echo $cart_qty; ?></th>
@@ -134,12 +137,20 @@ $sql="SELECT `pid`,`qty` FROM `orderdetail` WHERE `orderid`='$oid'";
                                                     </tr>
                         </tbody>
                 </table>
+                <?php $data.="<tr>
+                            <th colspan=4></th>
+                            <th class='text-center'>Total<br>Quantity:".$cart_qty."</th>
+                            <th class='text-center'>Cart Total :<br>".$cart_total."</th>
+                                                    </tr>
+                        </tbody>
+                </table>";
+                $_SESSION['data']=$data;
+                ?>
                 <div class="d-flex flex-row-reverse bd-highlight my-4">
                     <!-- Button trigger modal -->
-                    <form action="" method="post">
-                    <button type="submit" name="savepdf" class="btn btn-primary">
+                    <a href="billpdf.php"><button type="submit" name="savepdf" class="btn btn-primary">
                     Save
-                    </button></form>&nbsp;&nbsp;&nbsp;
+                    </button></a>&nbsp;&nbsp;&nbsp;
                     <a href="home.php"><button type="button" class="btn btn-primary" >
                     Close
                     </button>
