f1e5ce4049f1ea31725fc507105dc758cf893646
HrujulThumar22 <hrujul.a.thumar@gmail.com>
2021-03-14 12:53:07 +0530

    billpdf.php updated

--- billpdf.php
+++ billpdf.php
@@ -1,15 +1,19 @@
 <?php
-session_start();
-if( $_SESSION['loggedin']!="user" || !isset($_SESSION['email']))
+    session_start();
+    require_once('./classes/user.php');
+    if(isset($_SESSION['user']))
     {
-        header('location:login.php');
+        $user=unserialize($_SESSION['user']);
+        if($user->category!='user')
+            header('location:logout.php');
     }
-$data=$_SESSION['data'];
-#$ss=$_SESSION['ss'];
-require_once '../vendor/autoload.php';
-$mpdf=new \Mpdf\Mpdf();
-$ss= file_get_contents('style.css'); // external css
-$mpdf->WriteHTML($ss,1);
-$mpdf->WriteHTML($data,2);
-$mpdf->Output('invoice.pdf','D');
+    else header('location:login.php');
+    $data=$_SESSION['data'];
+    #$ss=$_SESSION['ss'];
+    require_once '../vendor/autoload.php';
+    $mpdf=new \Mpdf\Mpdf();
+    $ss= file_get_contents('style.css'); // external css
+    $mpdf->WriteHTML($ss,1);
+    $mpdf->WriteHTML($data,2);
+    $mpdf->Output('invoice.pdf','D');
 ?>
