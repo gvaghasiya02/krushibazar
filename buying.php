cced30ce9564d764add6911060bb0052d3a3d1fd
HrujulThumar22 <hrujul.a.thumar@gmail.com>
2021-03-14 12:54:51 +0530

    buying.php updated

--- buying.php
+++ buying.php
@@ -1,12 +1,17 @@
 <?php
 $insertSuccess=false;
     session_start();
-    if( $_SESSION['loggedin']!="user" || !isset($_SESSION['email']))
+    require_once('./classes/user.php');
+    if(isset($_SESSION['user']))
     {
-        header('location:login.php');
+        $user=unserialize($_SESSION['user']);
+        if($user->category!='user')
+            header('location:logout.php');
     }
+    else header('location:login.php');
+
     require_once 'config.php';
-    $userid=$_SESSION['id']; 
+    $userid=$user->userid; 
     $tt="All";
     $sql="SELECT `pname`,`category`,`pinfo`,`price`,`image`,`userid`,`pid`,`qty` FROM product where userid!=$userid and qty!=0";
     if(isset($_POST["submit"]))
@@ -55,10 +60,10 @@ $insertSuccess=false;
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item"><a class="nav-link" href="home.php">Home</a></li>
+                <li class="nav-item active"><a class="nav-link" href="home.php">Home</a></li>
                 <li class="nav-item"><a class="nav-link" href="sale.php">Selling Crops</a></li>
-                <li class="nav-item active"><a class="nav-link" href="buying.php">Buying Products</a></li>
-                <li class="nav-item"><a class="nav-link" href="profile.php">logged in as:<?php echo $_SESSION['email'];?></a></li>
+                <li class="nav-item"><a class="nav-link" href="buying.php">Buying Products</a></li>
+                <li class="nav-item"><a class="nav-link" href="profile.php">logged in as:<?php echo $user->email;?></a></li>
             </ul>
             <ul class="nav navbar-nav">
             <li class="nav-item"><a  class="nav-link" href="cart.php">My Cart</a></li>
