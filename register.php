1b1954b1a1100499d3e9598cb71a0aa3496b28b9
HrujulThumar22 <hrujul.a.thumar@gmail.com>
2021-03-19 12:29:40 +0530

    register repopulate

--- register.php
+++ register.php
@@ -1,15 +1,15 @@
 <?php
 $success=true;
 require_once("config.php");
-$email=$password=$confirm_pass="";
-$err="<br>";
+$email=$password=$confirm_pass=$firstname=$lastname=$dob=$address=$state=$city=$phonenumber=$gender="";
+$err=$email_err=$pass_err=$fname_err=$lname_err=$dob_err=$address_err=$state_err=$city_err=$pno_err=$gender_err="";
 
     if($_SERVER['REQUEST_METHOD']=="POST")
     {
         //Check is username is empty
         if(empty(trim($_POST['email'])))
         {
-            $err.="Email cannot be empty<br>";
+            $email_err.="Email cannot be empty";
         }
         else
         {
@@ -20,7 +20,7 @@ $err="<br>";
             //var_dump($result);
             if($result->num_rows>0)
             {
-                $err.="User with Same Email already taken<br>";
+                $email_err.="User with Same Email already taken";
                 $success=false;
             }
             else
@@ -30,20 +30,20 @@ $err="<br>";
         }
         
 
-        //check for pass
+        //check for password
         if(empty(trim($_POST['password'])))
         {
-            $err.="Password cannot be empty<br>";
+            $pass_err.="Password cannot be empty";
             $success=false;
         }
         elseif(strlen(trim($_POST['password']))<5)
         {
-            $err.="Password cannot be less than 5 characters<br>";
+            $pass_err.="Password cannot be less than 5 characters";
             $success=false;
         }
         elseif(trim($_POST['password']) != trim($_POST['confirm_password']))
         {
-            $err.="Password should match<br>";
+            $pass_err.="Password should match";
             $success=false;
         }
         else
@@ -51,37 +51,98 @@ $err="<br>";
             $password=password_hash(trim($_POST['password']),PASSWORD_DEFAULT);
         }
 
-        //Check if other fields are present
-        if(empty(trim($_POST['firstname'])) || empty(trim($_POST['lastname'])) || empty(trim($_POST['dob'])) || empty(trim($_POST['address'])) || empty(trim($_POST['state'])) || empty(trim($_POST['city'])) || empty(trim($_POST['phonenumber'])) || empty($_POST['gender']))
+        //Check for Firstname
+        if(empty(trim($_POST['firstname'])))
         {
-            $err.="Enter all details<br>";
+            $fname_err.="Enter First Name";
+            $success=false;
+        }
+        else{
+            $firstname=$_POST['firstname'];
+        }
+
+        //Check for Lastname
+        if(empty(trim($_POST['lastname'])))
+        {
+            $lname_err.="Enter Last Name";
+            $success=false;
+        }
+        else{
+            $lastname=$_POST['lastname'];
+        }
+
+        //Check for DOB
+        if(empty(trim($_POST['dob'])))
+        {
+            $dob_err.="Enter Date of Birth";
+            $success=false;
+        }
+        elseif(round((strtotime(date("D F Y"))-(strtotime($_POST['dob'])))/(31540000))<18)
+        {
+            $dob_err.="You Should be 18";
+            $success=false;
+        }
+        else{
+            $dob=$_POST['dob'];
+        }
+
+        //Check for Address
+        if(empty(trim($_POST['address'])))
+        {
+            $address_err.="Enter Address";
+            $success=false;
+        }
+        else{
+            $address=$_POST['address'];
+        }
+
+        //Check for State
+        if(empty(trim($_POST['state'])))
+        {
+            $state_err.="Enter State";
+            $success=false;
+        }
+        else{
+            $state=$_POST['state'];
+        }
+
+        //Check for City
+        if(empty(trim($_POST['city'])))
+        {
+            $city_err.="Enter City";
+            $success=false;
+        }
+        else{
+            $city=$_POST['city'];
+        }
+
+        //Check for Phone Number
+        if(empty(trim($_POST['phonenumber'])))
+        {
+            $pno_err.="Enter Phone Number";
             $success=false;
         }
         elseif(strlen((string)$_POST['phonenumber'])!=10)
         {
-            $err.="Enter correct PhoneNumber<br>";
+            $pno_err.="Phonenumber should be of 10 digits";
             $success=false;
         }
-        elseif(round((strtotime(date("D F Y"))-(strtotime($_POST['dob'])))/(31540000))<18)
-        {   echo round((strtotime(date("D F Y"))-(strtotime($_POST['dob'])))/(31540000));
-            $err.="Enter date correct<br>";
-            $success=false;
-        }
-        else
-        {
-            $firstname=$_POST['firstname'];
-            $lastname=$_POST['lastname'];
-            $dob=$_POST['dob'];
-            $address=$_POST['address'];
-            $state=$_POST['state'];
-            $city=$_POST['city'];
+        else{
             $phonenumber=$_POST['phonenumber'];
+        }
+
+        //Check for Gender
+        if(!isset($_POST['gender']))
+        {
+            $gender_err.="Enter Gender";
+            $success=false;
+        }
+        else{
             $gender=$_POST['gender'];
-            #echo $dob;
         }
 
         //if no error
-        if($err=="<br>")
+        if(empty($err) && empty($email_err) && empty($pass_err) && empty($fname_err) && empty($lname_err) && empty($dob_err) && empty($address_err) && empty($city_err) && empty($pno_err) && empty($gender_err))
         {
             $sql="INSERT INTO user(email,password,firstname,lastname,dob,address,state,city,phonenumber,gender) VALUES ('$email','$password','$firstname','$lastname','$dob','$address','$state','$city','$phonenumber','$gender')";
             $stmt=$conn->query($sql);
@@ -93,7 +154,6 @@ $err="<br>";
                 echo "Error: " . $sql . "<br>" . $conn->error;
                 $success=false;
             }
-            $conn->close();
         }
     }
     $conn->close();
@@ -123,68 +183,79 @@ $err="<br>";
         </div>";
     }
 ?>
-    <div class="container shadow mt-4">
+    <div class="container shadow my-4">
     <h1 class="text-primary">Register Here</h1>
+    <span class='text-danger'>* field are required</span>
     <form action="" method="post">
         <div class="form-row">
             <div class="form-group col-md-6">
-                <label for="firstname">FirstName</label>
-                <input class="form-control" type="text" name="firstname" id="firstname" placeholder="Enter First Name">
+                <label for="firstname">FirstName</label><span class='text-danger'>*</span>
+                <input class="form-control" type="text" name="firstname" id="firstname" value='<?php echo $firstname; ?>' placeholder="Enter First Name">
+                <span class='text-danger'><?php echo $fname_err; ?></span>
             </div>
             <div class="form-group col-md-6">
-                <label for="lastname">LastName</label>
-                <input class="form-control" type="text" name="lastname" id="lastname" placeholder="Enter Last Name">
+                <label for="lastname">LastName</label><span class='text-danger'>*</span>
+                <input class="form-control" type="text" name="lastname" id="lastname" value='<?php echo $lastname; ?>' placeholder="Enter Last Name">
+                <span class='text-danger'><?php echo $lname_err; ?></span>
             </div>
         </div>
         <div class="form-row">
             <div class="form-group col-md-6">
-                <label for="dob">Date Of Birth</label>
-                <input class="form-control" type="date" name="dob" id="dob" placeholder="Enter DOB(age must be 18)">
+                <label for="dob">Date Of Birth</label><span class='text-danger'>*</span>
+                <input class="form-control" type="date" name="dob" id="dob" value='<?php echo $dob; ?>' placeholder="Enter DOB(age must be 18)">
+                <span class='text-danger'><?php echo $dob_err; ?></span>
             </div>
             <div class="form-group col-md-6">
-                <label for="phonenumber">Phone Number</label>
-                <input class="form-control" type="number" name="phonenumber" id="phonenumber" placeholder="Enter your Phone Number">
+                <label for="phonenumber">Phone Number</label><span class='text-danger'>*</span>
+                <input class="form-control" type="number" name="phonenumber" id="phonenumber" value='<?php echo $phonenumber; ?>' placeholder="Enter your Phone Number">
+                <span class='text-danger'><?php echo $pno_err; ?></span>
             </div>
         </div>
         <div class="form-row">
             <div class="form-group col-md-12">
-                <label for="address">Address</label>
-                <input class="form-control" type="textarea" name="address" id="address" placeholder="Enter your Address">
+                <label for="address">Address</label><span class='text-danger'>*</span>
+                <input class="form-control" type="textarea" name="address" id="address" value='<?php echo $address; ?>' placeholder="Enter your Address">
+                <span class='text-danger'><?php echo $address_err; ?></span>
             </div>
         </div >
         <div class="form-row">
             <div class="form-group col-md-6">
-                <label for="state">State</label>
-                <input class="form-control" type="text" name="state" id="state" placeholder="Enter State">
+                <label for="state">State</label><span class='text-danger'>*</span>
+                <input class="form-control" type="text" name="state" id="state" value='<?php echo $state; ?>' placeholder="Enter State">
+                <span class='text-danger'><?php echo $state_err; ?></span>
             </div>
             <div class="form-group col-md-6">
-                <label for="city">City</label>
-                <input class="form-control" type="text" name="city" id="city" placeholder="Enter City">
+                <label for="city">City</label><span class='text-danger'>*</span>
+                <input class="form-control" type="text" name="city" id="city" value='<?php echo $city; ?>' placeholder="Enter City">
+                <span class='text-danger'><?php echo $city_err; ?></span>
             </div>
         </div>
         <div class="form-row">
             <div class="form-group col-md-6">
-                <label for="male">Gender</label>
-                <input class="form-control-md" type="radio" name="gender" value="Male">Male
-                <input class="form-control-md" type="radio" name="gender" value="Female">Female
+                <label for="male">Gender</label><span class='text-danger'>*</span>
+                <input class="form-control-md" type="radio" name="gender" <?php if(!empty($gender) && $gender=="Male") echo 'checked'; ?> value="Male">Male
+                <input class="form-control-md" type="radio" name="gender" <?php if(!empty($gender) && $gender=="Female") echo 'checked'; ?>value="Female">Female
+                <span class='text-danger'><?php echo $gender_err; ?></span>
             </div>
         </div>
         <div class="form-row">
             <div class="form-group col-md-12">
-                <label for="email">Email</label>
-                <input type="email" class="form-control" name="email" id="email" placeholder="Enter Email">
+                <label for="email">Email</label><span class='text-danger'>*</span>
+                <input type="email" class="form-control" name="email" id="email" value='<?php echo $email; ?>' placeholder="Enter Email">
+                <span class='text-danger'><?php echo $email_err; ?></span>
             </div>
             <div class="form-group col-md-6">
-                <label for="password">Password</label>
+                <label for="password">Password</label><span class='text-danger'>*</span>
                 <input type="password" class="form-control" name="password" id="password" placeholder="Enter Password">
+                <span class='text-danger'><?php echo $pass_err; ?></span>
             </div>
             <div class="form-group col-md-6">
-                <label for="confirm_password">Confirm Password</label>
+                <label for="confirm_password">Confirm Password</label><span class='text-danger'>*</span>
                 <input type="password" class="form-control" name="confirm_password" id="confirm_password" placeholder="Re-Enter Password">
             </div>
         </div>
         <button type="submit" class="btn btn-primary col-md-12">Sign Up</button>
-        <label for="register">Aleady a Member?</label><a href="login.php" id="register">Sign In Here</a>
+        <label for="register">Already a Member?</label><a href="login.php" id="register">Sign In Here</a>
     </form>
     </div>
     <!-- Optional JavaScript -->
