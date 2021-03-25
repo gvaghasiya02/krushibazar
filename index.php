bdbd8f4f9731baa0e89455d9d6594bc1754928ad
GV <gvaghasiya02@gmail.com>
2021-03-25 08:52:19 +0530

    kk

--- index.php
+++ index.php
@@ -22,39 +22,71 @@
             </ul>
         </div>
     </nav>
-    <div class="container">
-        <p>Krushi India Corporation established in the year 2012. Let's have a great privilege to introduce our self as The Quality Manufacturer of Organic Manure.
+    <div class="container mt-5">
+      <!-- welcome block -->
 
-“A New Idea for Successful farming”
-“Our aim is to increase the Fertility of Land and Agricultural Productivity”
-“We Believe in Quality Product”
-We have an established advanced infrastructure. Our processing unit is installed with latest machines and innovative tools so that the products are manufactured with utmost ease. These machines are regularly serviced for hassle-free operations and are upgraded in compliance with the prevailing technological advancement. Moreover, we also ensure that all the processes are completed at our infrastructure in a hassle free manner.
+      <!-- end welcome block -->
+      <!-- fresh flavor block -->
+      <div class="fresh-flavour-block welcome-main-block">
+        <div class="row">
+          <div class="col-md-6">
+            <h1 class="fresh-flavour-title new-h4">ABOUT US</h1>
+            <p><em style="font-size: 15px;"><strong>Krushi India</strong></em> Corporation established in the year 2012. Let's have a great privilege to introduce our self as The Quality Manufacturer of Organic Manure.</p>
+            <h4>“A New Idea for Successful farming”</h4>
+            <h4>“Our aim is to increase the Fertility of Land and Agricultural Productivity”</h4>
+            <h4>“We Believe in Quality Product”</h4>
+            <p>We have an established advanced infrastructure. Our processing unit is installed with latest machines and innovative tools so that the products are manufactured with utmost ease. 
+              These machines are regularly serviced for hassle-free operations and are upgraded in compliance with the prevailing technological advancement. 
+              Moreover, we also ensure that all the processes are completed at our infrastructure in a hassle free manner.</p>
+            <p>As we are quality centric firm, we make all our efforts to maintain consistent quality of our 
+              <em><strong>CALSIPHOS BIO NPK, ORGANIC MANURE, PROM, MAGNESIUM SULPHATE, BENTONITE SULPHAR 90%, MICRONUTRIENTS, ZYME</strong></em> etc. To ensure optimum quality of the products, we have established an advanced quality testing unit that is facilitated with latest testing tools. Also, our quality controllers test the entire product range at every stage, right from the procurement of raw material to final dispatch. We conduct quality test on different parameter to ensure the quality of the products before supplying to the clients.</p>
 
-As we are quality centric firm, we make all our efforts to maintain consistent quality of our CALSIPHOS BIO NPK, ORGANIC MANURE, PROM, MAGNESIUM SULPHATE, BENTONITE SULPHAR 90%, MICRONUTRIENTS, ZYME etc. To ensure optimum quality of the products, we have established an advanced quality testing unit that is facilitated with latest testing tools. Also, our quality controllers test the entire product range at every stage, right from the procurement of raw material to final dispatch. We conduct quality test on different parameter to ensure the quality of the products before supplying to the clients.
-Because of its Quality Consistency and Cost-Effectiveness, 'CALSIPHOS' has received an awesome response from the world of Farming. And 'CALSIPHOS' is now the Quality Brand of Gujarat.
+          </div>
+          <div class="col-md-6">
+            <div class="row">
+              <div class="col-xs-6">
+                <div class="fresh-flavour-img fresh-flavour-img-one">
+                  <img src="./images/fresh-flavor-1.jpg" class="img-responsive" alt="fresh-flavor">
+                </div>
+                <div class="fresh-flavour-img fresh-flavour-img-two">
+                  <img src="./images/fresh-flavor-2.jpg" class="img-responsive" alt="fresh-flavor">
+                </div>
+              </div>
+              <div class="col-xs-6">
+                <div class="fresh-flavour-img">
+                  <img src="./images/fresh-flavor-3.jpg" class="img-responsive" alt="fresh-flavor">
+                </div>
+                <div class="fresh-flavour-img">
+                  <img src="./images/fresh-flavor-4.jpg" class="img-responsive" alt="fresh-flavor">
+                </div>
+                <div class="fresh-flavour-img">
+                  <img src="./images/fresh-flavor-5.jpg" class="img-responsive" alt="fresh-flavor">
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-12">
+            <p>Because of its Quality Consistency and Cost-Effectiveness, 'CALSIPHOS' has received an awesome response from the world of Farming. And 'CALSIPHOS' is now the Quality Brand of Gujarat.</p>
+            <p>Our team of workers is dexterous and dedicated towards the goal of the organization. We have administrators, Dealers, warehouse personnel, transportation personnel, quality controllers, packaging staff and sales executives. They are well versed with their individual work and committed to our customers for providing them with high quality product. We have a reliable vendor base and their track records are constantly checked by our professionals for delivering products in tune with the international standards and prevalent market trends.</p>
+            <p>Our mentor, Mr. Krupesh Patel, is the constant source of inspiration for the organization. His rich knowledge base has enabled us to achieve heights of success in a short period of time.</p>
+            <p>Krushi India Corporation has always believed that the people and the community within which it operates are the cornerstones for its success and as such is actively involved in Corporate Social Responsibility (CSR) activities aimed at social welfare.</p>
+            <p>We are supplying our products to the Government of India.</p>
+            <h2 class="fresh-flavour-title new-h4">Quality Policy</h2>
+            <p>At KRUSHI INDIA CORPORATION the management and the employees are committed to secure a long-term partnership with customers by providing world class Products and services that exceed expectations.</p>
+            <ul class="about-ul-s">
+              <li>We shall conduct quality-related activities in order to provide customers with the best possible products.</li>
+              <li>We shall observe all relevant laws, regulations, and contracts, and make efforts to continuously improve quality. By ensuring the quality and safety of our products, we will meet our customers' requirements and increase customer satisfaction.</li>
+              <li>We shall establish a quality management system and work to continuously improve the system.</li>
+              <li>We shall do our best to provide products of consistent quality. We shall investigate and identify the root cause of any inconsistencies based on data, and make all required improvements in order to ensure consistent quality.</li>
+            </ul>
+          </div>
 
-Our team of workers is dexterous and dedicated towards the goal of the organization. We have administrators, Dealers, warehouse personnel, transportation personnel, quality controllers, packaging staff and sales executives. They are well versed with their individual work and committed to our customers for providing them with high quality product. We have a reliable vendor base and their track records are constantly checked by our professionals for delivering products in tune with the international standards and prevalent market trends.
 
-Our mentor, Mr. Krupesh Patel, is the constant source of inspiration for the organization. His rich knowledge base has enabled us to achieve heights of success in a short period of time.
 
-Krushi India Corporation has always believed that the people and the community within which it operates are the cornerstones for its success and as such is actively involved in Corporate Social Responsibility (CSR) activities aimed at social welfare.
 
-We are supplying our products to the Government of India.QUALITY POLICY
-At KRUSHI INDIA CORPORATION the management and the employees are committed to secure a long-term partnership with customers by providing world class Products and services that exceed expectations.
-
-We shall conduct quality-related activities in order to provide customers with the best possible products.
-We shall observe all relevant laws, regulations, and contracts, and make efforts to continuously improve quality. By ensuring the quality and safety of our products, we will meet our customers' requirements and increase customer satisfaction.
-We shall establish a quality management system and work to continuously improve the system.
-We shall do our best to provide products of consistent quality. We shall investigate and identify the root cause of any inconsistencies based on data, and make all required improvements in order to ensure consistent quality.</p>
-        <h4>Important links</h4> 	  
-        <ul>
-            <li><a href="/website-ministry-agriculture-farmers-welfare">Website of Ministry of Agriculture &amp; Farmers Welfare</a></li>
-            <li><a href="/departments-agriculture-states-and-union-territories">Departments of Agriculture of states and Union Territories</a></li>
-            <li><a href="/website-department-animal-husbandry-dairying-and-fisheries-0">Website of Department of Animal Husbandry Dairying and Fisheries</a></li><li><a href="/website-directorate-cashewnut-cocoa-development">Website of Directorate of Cashewnut &amp; Cocoa Development</a></li>
-            <li><a href="/farmers-portal-india-department-agriculture-and-cooperation">Farmers&#039; Portal of India by Department of Agriculture and Cooperation</a></li>	  
-        </ul> 
+        </div>
+      </div>
     </div>
-    
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
